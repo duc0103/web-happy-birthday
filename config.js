f4f61f29b7df6cb7925db80542b16c68c67e41b1
duc0103 <leducdhcn@gmail.com>
2021-07-02 01:49:47 +0700

    complete web happy birthday

---
+++ config.js
@@ -0,0 +1,25 @@
+const CONFIG = {
+    introTitle: 'Sinh nhật vui vẻ nghe Giang xinh gái ',
+    introTitle2:'Xinh đẹp và thành công nhá ',
+    introTitle3:'Xinh như ảnh ni là được nì',
+    introDesc: `Happy birthday to you
+    Happy birthday to you
+    Happy birthday, happy birthday, happy birthday to you`,
+    introDesc2:` Chỉ còn vài ngày nữa thôi là cuộc đời em sẽ bước qua trang mới cho nên hãy tập trung cao độ để thi cử thật tốt nhé!`,
+    introDesc3:`chúc thêm may mắn như cỏ 4 lá  nha`
+    ,
+    btnIntro: 'ấn vô để tiếp tục xem nè',
+    btnIntro2:'tiếp nè',
+    btnIntro3:'có ảnh dìm luôn',
+    title: 'Bây giờ có thích coi ảnh dìm hông ta 😙',
+    desc: 'nói gì thì nói nhưng sinh nhật là phải có ảnh dìm',
+    btnYes: 'coi thì coi sợ gì',
+    btnNo: 'Không, xấu nắm',
+    question:'hihi nếu hay thì gửi cho tui lời nhắn tin nha',
+    btnReply: 'Gửi cho anh  <3',
+    reply: 'hay lắm luôn á trời =)',
+    mess: 'oke nhé 🥰. cấm giận  😘😘',
+    messDesc: 'nhưng nay không đúng sinh nhật , hôm sau đúng có quà nha',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100009374725970' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
